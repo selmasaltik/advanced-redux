41ed2c9e4a4a9a05b94636da592370a91e4c9529
selmasaltik <selmasaltiik@gmail.com>
2024-10-23 01:56:41 +0300

    Finalizing the Fetching Logic

--- 19-react-redux/src/store/cart-actions.js
+++ 19-react-redux/src/store/cart-actions.js
@@ -19,7 +19,10 @@ export const fetchCartData = () => {
 
         try {
            const cartData = await fetchData();
-           dispatch(cartActions.replaceCart(cartData));
+           dispatch(cartActions.replaceCart({
+            items: cartData.items || [],
+            totalQuantity: cartData.totalQuantity
+           }));
         } catch (error) {
             dispatch(uiActions.showNotification({
                 status: 'error',
@@ -42,7 +45,10 @@ export const sendCartData = (cart) => {
         const sendRequest = async () => {
             const response = await fetch('https://react-http-e48ff-default-rtdb.firebaseio.com/cart.json', {
                 method: 'PUT',
-                body: JSON.stringify(cart)
+                body: JSON.stringify({
+                    items: cart.items,
+                    totalQuantity: cart.totalQuantity
+                })
             });
     
             if(!response.ok) {
